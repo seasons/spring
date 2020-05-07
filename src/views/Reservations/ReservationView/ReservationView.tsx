97ac5b693e8333b5c3103dd071287aad88b495b3
Luc Succes <l2succes@gmail.com>
2020-05-06 22:50:09 -0400

    Hook up process modal

--- src/views/Reservations/ReservationView/ReservationView.tsx
+++ src/views/Reservations/ReservationView/ReservationView.tsx
@@ -8,10 +8,12 @@ import ViewModuleIcon from "@material-ui/icons/ViewModule"
 import ListIcon from "@material-ui/icons/List"
 import { ToggleButtonGroup, ToggleButton } from "@material-ui/lab"
 import MoveToInboxIcon from "@material-ui/icons/MoveToInbox"
+import { PricingModal } from "components/PricingModal"
 
 export const ReservationView = ({ match, history, props }) => {
   const { id } = match.params
   const [mode, setMode] = useState("grid")
+  const [showModal, openModal] = useState(false)
 
   const { data, loading, error } = useQuery({
     type: "getOne",
@@ -37,46 +39,55 @@ export const ReservationView = ({ match, history, props }) => {
   }
 
   return (
-    <Container maxWidth={false}>
-      <Box my={2}>
-        <Header
-          title="Reservation Details"
-          breadcrumbs={[
-            {
-              title: "Reservations",
-              url: "/reservations",
-            },
-            { title: `Reservation: ${data.reservationNumber}`, url: `/reservations/${data.reservationNumber}` },
-          ]}
-        />
+    <>
+      <Container maxWidth="lg">
         <Box my={2}>
-          <ReservationInfo reservation={data} />
-        </Box>
-        <Box display="flex" alignItems="center" justifyContent="space-between" flexWrap="wrap" mb={2}>
-          <Typography variant="h3">Items</Typography>
-          <Box display="flex" alignItems="center">
-            <ToggleButtonGroup exclusive onChange={handleModeChange} size="small" value={mode}>
-              <ToggleButton value="grid">
-                <ViewModuleIcon />
-              </ToggleButton>
-              <ToggleButton value="list">
-                <ListIcon />
-              </ToggleButton>
-            </ToggleButtonGroup>
+          <Header
+            title="Reservation Details"
+            breadcrumbs={[
+              {
+                title: "Reservations",
+                url: "/reservations",
+              },
+              { title: `Reservation: ${data.reservationNumber}`, url: `/reservations/${data.reservationNumber}` },
+            ]}
+          />
+          <Box my={2}>
+            <ReservationInfo reservation={data} />
           </Box>
+          <Box display="flex" alignItems="center" justifyContent="space-between" flexWrap="wrap" mb={2}>
+            <Typography variant="h3">Items</Typography>
+            <Box display="flex" alignItems="center">
+              <ToggleButtonGroup exclusive onChange={handleModeChange} size="small" value={mode}>
+                <ToggleButton value="grid">
+                  <ViewModuleIcon />
+                </ToggleButton>
+                <ToggleButton value="list">
+                  <ListIcon />
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Box>
+          </Box>
+          <Grid container spacing={2}>
+            {data.products.map(product => (
+              <Grid
+                item
+                lg={mode === "grid" ? 3 : 12}
+                md={mode === "grid" ? 4 : 12}
+                sm={mode === "grid" ? 4 : 12}
+                xs={12}
+              >
+                <ProductCard product={product} />
+              </Grid>
+            ))}
+          </Grid>
+          <Fab color="primary" variant="extended" onClick={() => openModal(true)}>
+            <MoveToInboxIcon />
+            <Box ml={1}>Process Return</Box>
+          </Fab>
         </Box>
-        <Grid container spacing={2}>
-          {data.products.map(product => (
-            <Grid item md={mode === "grid" ? 4 : 12} sm={mode === "grid" ? 4 : 12} xs={12}>
-              <ProductCard product={product} />
-            </Grid>
-          ))}
-        </Grid>
-        <Fab color="primary" variant="extended">
-          <MoveToInboxIcon />
-          <Box ml={1}>Process Return</Box>
-        </Fab>
-      </Box>
-    </Container>
+      </Container>
+      <PricingModal open={showModal} onClose={() => openModal(false)} />
+    </>
   )
 }
