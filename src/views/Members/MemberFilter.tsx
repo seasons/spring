a345b36eb5ff1171b5dd1eafe6f00a1a5bd07177
Luc Succes <l2succes@gmail.com>
2020-05-25 22:51:15 -0400

    Wrap list in Card component

--- src/views/Members/MemberFilter.tsx
+++ src/views/Members/MemberFilter.tsx
@@ -1,11 +1,14 @@
 import React from "react"
+import { Box } from "@material-ui/core"
 import { Filter, ReferenceArrayInput, SelectArrayInput, TextInput } from "@seasons/react-admin"
 
 export const MemberFilter = props => (
-  <Filter {...props}>
-    <TextInput label="Search" source="user.firstName_contains" alwaysOn />
-    <ReferenceArrayInput source="customer" reference="Customer">
-      <SelectArrayInput />
-    </ReferenceArrayInput>
-  </Filter>
+  <Box px={2}>
+    <Filter {...props}>
+      <TextInput label="Search" source="user.firstName_contains" alwaysOn />
+      <ReferenceArrayInput source="customer" reference="Customer">
+        <SelectArrayInput />
+      </ReferenceArrayInput>
+    </Filter>
+  </Box>
 )
