420d93065aada5415b65e6cec94dfccdd457448e
Luc Succes <l2succes@gmail.com>
2020-05-26 14:08:36 -0400

    Add Menu to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,36 @@
 import React from "react"
 import { Link as RouterLink } from "react-router-dom"
-import { Button, Grid, Typography, Box, Breadcrumbs, Link } from "@material-ui/core"
-import NavigateNextIcon from "@material-ui/icons/NavigateNext"
+import { Button, Typography, Box, Breadcrumbs, Link, Menu, MenuItem, IconButton, styled } from "@material-ui/core"
 import { Spacer, Text } from "components"
+
+import NavigateNextIcon from "@material-ui/icons/NavigateNext"
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz"
+import { colors } from "theme"
 
 export interface HeaderProps {
   className?: string
   title: string | JSX.Element
   subtitle?: string | JSX.Element
   primaryButton?: {
-    text: String
+    text: string
     icon?: JSX.Element
     action?: () => void
   }
   breadcrumbs?: Array<{ url: string; title: string }>
+  menuItems?: Array<{ text: string; action?: () => void }>
 }
 
-export const Header: React.FC<HeaderProps> = ({ title, subtitle, primaryButton, breadcrumbs }) => {
+export const Header: React.FC<HeaderProps> = ({ title, subtitle, primaryButton, breadcrumbs, menuItems }) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <Box display="flex" mt={4} mb={3}>
       <Box flexGrow={1}>
@@ -52,15 +66,40 @@ export const Header: React.FC<HeaderProps> = ({ title, subtitle, primaryButton, 
           </Text>
         )}
       </Box>
-      <Box>
+      <Box display="flex" alignItems="center">
         {primaryButton && (
-          <Grid item>
+          <Box>
             <Button color="primary" variant="contained" onClick={primaryButton.action} startIcon={primaryButton.icon}>
               {primaryButton.text}
             </Button>
-          </Grid>
+          </Box>
+        )}
+        {menuItems && (
+          <Box mx={2}>
+            <BorderedIconButton onClick={handleClick} size="small">
+              <MoreHorizIcon />
+            </BorderedIconButton>
+            <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+              {menuItems.map((item, i) => (
+                <MenuItem
+                  key={`menuitem-${item.text + i}`}
+                  onClick={() => {
+                    handleClose()
+                    item?.action?.()
+                  }}
+                >
+                  {item.text}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         )}
       </Box>
     </Box>
   )
 }
+
+const BorderedIconButton = styled(IconButton)({
+  border: `1px solid ${colors.grey[300]}`,
+  padding: "5px",
+})
