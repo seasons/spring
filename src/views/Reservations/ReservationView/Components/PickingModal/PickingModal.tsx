23c8da769ecaddd39e30abf2e4176b691b9cbbc4
Luc Succes <l2succes@gmail.com>
2020-06-05 00:20:09 -0400

    Only pick reservable items

--- src/views/Reservations/ReservationView/Components/PickingModal/PickingModal.tsx
+++ src/views/Reservations/ReservationView/Components/PickingModal/PickingModal.tsx
@@ -24,7 +24,8 @@ type ProductStates = { [key: string]: ProductState }
 
 export const PickingModal: React.FC<PickingModalProps> = ({ disableButton, open, onSave, onClose, reservation }) => {
   const barcodeMaps = {}
-  reservation.products.forEach(product => {
+  const availableProducts = reservation.products.filter(a => a.inventoryStatus === "Reservable")
+  availableProducts.forEach(product => {
     barcodeMaps[product.barcode] = {
       productUID: product.seasonsUID,
       picked: false,
@@ -138,7 +139,7 @@ export const PickingModal: React.FC<PickingModalProps> = ({ disableButton, open,
             </Box>
           )}
           <Box mt={2} mb={2}>
-            {reservation.products.map(product => (
+            {availableProducts.map(product => (
               <Box mb={2} key={`product-card-${product.id}`}>
                 <PickingProductCard
                   product={product}
