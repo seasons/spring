1a5c89067f7de67465f017fdd5ba546add940aa2
Luc Succes <l2succes@gmail.com>
2020-06-17 10:46:39 -0400

    Updates generated types

--- src/generated/customer.ts
+++ src/generated/customer.ts
@@ -3,7 +3,14 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { Plan, CustomerStatus, InvoiceStatus, CreditNoteReasonCode, CreditNoteStatus } from "./globalTypes"
+import {
+  Plan,
+  CustomerStatus,
+  InvoiceStatus,
+  CreditNoteReasonCode,
+  CreditNoteStatus,
+  ReservationStatus,
+} from "./globalTypes"
 
 // ====================================================
 // GraphQL fragment: customer
@@ -67,7 +74,7 @@ export interface customer_reservations {
   id: string
   reservationNumber: number
   shipped: boolean
-  status: string
+  status: ReservationStatus
   shippedAt: any | null
   receivedAt: any | null
   products: customer_reservations_products[]
