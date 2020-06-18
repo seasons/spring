06cd4d68d44110aadb52d0ec401e8235d19ddeee
Luc Succes <l2succes@gmail.com>
2020-06-18 10:34:45 -0400

    Standardize tabs in Member view

--- src/views/Members/History/index.tsx
+++ src/views/Members/History/index.tsx
@@ -1,8 +1,7 @@
 import { ProductItemsField, SinceDateField, StatusField, ViewEntityField } from "fields"
 import React from "react"
 import { Datagrid, TextField } from "@seasons/react-admin"
-
-import { Container } from "@material-ui/core"
+import { Card } from "@material-ui/core"
 
 import { MemberSubViewProps } from "../interfaces"
 
@@ -12,17 +11,15 @@ export const HistoryView: React.FC<MemberSubViewProps> = ({ member }) => {
   const defaultSort = { field: "reservationNumber", order: "ASC" }
 
   return (
-    <>
-      <Container maxWidth={false}>
-        <Datagrid ids={member?.reservations?.map(r => r.id)} data={normalizedReservations} currentSort={defaultSort}>
-          <TextField source="reservationNumber" label="Order #" />
-          <StatusField label="Order Status" />
-          <SinceDateField source="receivedAt" label="Date Placed" />
-          <SinceDateField source="shippedAt" label="Date Returned" />
-          <ProductItemsField source="productVariant.product.images" label="Items" />
-          <ViewEntityField entityPath="reservation" source="id" label="Actions" />
-        </Datagrid>
-      </Container>
-    </>
+    <Card>
+      <Datagrid ids={member?.reservations?.map(r => r.id)} data={normalizedReservations} currentSort={defaultSort}>
+        <TextField source="reservationNumber" label="Order #" />
+        <StatusField label="Order Status" />
+        <SinceDateField source="receivedAt" label="Date Placed" />
+        <SinceDateField source="shippedAt" label="Date Returned" />
+        <ProductItemsField source="productVariant.product.images" label="Items" />
+        <ViewEntityField entityPath="reservation" source="id" label="Actions" />
+      </Datagrid>
+    </Card>
   )
 }
--- src/views/Members/Personal/index.tsx
+++ src/views/Members/Personal/index.tsx
@@ -7,25 +7,15 @@ import { MemberSubViewProps } from "../interfaces"
 import { Lifestyle } from "./Lifestyle"
 import { Sizing } from "./Sizing"
 
-const useStyles = makeStyles<Theme>(theme => ({
-  root: {
-    padding: theme.spacing(5),
-  },
-}))
-
 export const PersonalView: React.FC<MemberSubViewProps> = ({ member, adminKey }) => {
-  const classes = useStyles()
-
   return (
-    <>
-      <Grid className={classes.root} container spacing={3}>
-        <Grid item lg={6} md={6} xl={6} xs={12}>
-          <Sizing adminKey={adminKey} member={member} />
-        </Grid>
-        <Grid item lg={6} md={6} xl={6} xs={12}>
-          <Lifestyle adminKey={adminKey} member={member} />
-        </Grid>
+    <Grid container spacing={3}>
+      <Grid item lg={6} md={6} xl={6} xs={12}>
+        <Sizing adminKey={adminKey} member={member} />
       </Grid>
-    </>
+      <Grid item lg={6} md={6} xl={6} xs={12}>
+        <Lifestyle adminKey={adminKey} member={member} />
+      </Grid>
+    </Grid>
   )
 }
