47d2dc54a5468b47afc60e7888fed3bd58d5f1e9
Kieran Gillen <kgillen@gmail.com>
2020-07-23 10:53:35 -0400

    Filters only items in warehouse

--- src/views/Reservations/ReservationView/Components/PickingModal/PickingModal.tsx
+++ src/views/Reservations/ReservationView/Components/PickingModal/PickingModal.tsx
@@ -35,9 +35,7 @@ type ProductStates = { [key: string]: ProductState }
 
 export const PickingModal: React.FC<PickingModalProps> = ({ disableButton, open, onSave, onClose, reservation }) => {
   const barcodeMaps = {}
-  const availableProducts = reservation?.products.filter(
-    product => product?.location?.slug === process.env.REACT_APP_MONSOON_ENDPOINT
-  )
+  const availableProducts = reservation?.products.filter(product => !!product?.warehouseLocation?.id)
   availableProducts.forEach(product => {
     barcodeMaps[product.barcode] = {
       productUID: product.seasonsUID,
