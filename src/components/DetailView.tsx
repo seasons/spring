a3a8d1ee73ef57596a9c986af5e13773a1842a22
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-09-09 17:46:06 -0400

    refactors member view to use abstract DetailView component

---
+++ src/components/DetailView.tsx
@@ -0,0 +1,114 @@
+import { ComponentError, Spacer } from "components"
+import React, { useState } from "react"
+import { Loading, useQueryWithStore } from "@seasons/react-admin"
+import { Redirect } from "react-router-dom"
+
+import { Box, colors, Container, Divider, Tab, Tabs, Theme } from "@material-ui/core"
+import { Snackbar, Header as BaseHeader } from "components"
+import { makeStyles } from "@material-ui/styles"
+import { History } from "history"
+import { SnackbarState } from "./Snackbar"
+
+const useStyles = makeStyles<Theme>(theme => ({
+  tabs: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(5),
+  },
+  divider: {
+    backgroundColor: colors.grey[300],
+  },
+  content: {
+    marginTop: theme.spacing(2),
+  },
+}))
+
+export interface DetailViewMatch {
+  params: { id: string; tab: string }
+  path: string // e.g /members/ckerk33lg041p07197gb16f8y/account
+}
+
+export interface TabRenderProps {
+  data: any
+  adminKey: string
+  recordID: string
+  match: DetailViewMatch
+}
+
+export interface HeaderRenderProps {
+  data: any
+}
+
+export interface DetailViewProps {
+  history: History
+  match: DetailViewMatch
+  resource: "Customer" | "Reservation"
+  renderHeader: (props: HeaderRenderProps) => React.Component
+  tabs: Array<{
+    value: string
+    label: string
+    render: (props: TabRenderProps) => React.Component
+  }>
+}
+
+export const DetailView: React.FunctionComponent<DetailViewProps> = ({
+  match,
+  history,
+  tabs,
+  resource,
+  renderHeader,
+  ...props
+}) => {
+  const classes = useStyles()
+  const { tab: currentTab, id: recordID } = match.params
+
+  const [snackbar, toggleSnackbar] = useState<SnackbarState>({
+    show: false,
+    message: "",
+    status: "success",
+  })
+
+  // Get the data
+  const { data, loading, error } = useQueryWithStore({
+    type: "getOne",
+    resource,
+    payload: { id: recordID },
+  })
+  if (loading) return <Loading />
+  if (error || !data) return <ComponentError />
+
+  // Edge Case
+  if (!tabs.map(t => t.value).includes(currentTab)) {
+    const pathStart = match.path.split("/")?.[1]
+    const firstTab = tabs?.[0]?.value
+    return <Redirect to={`/${pathStart}/${recordID}/${firstTab}`} />
+  }
+
+  // Main case
+  const renderCurrentTab = () => {
+    const activeTab = tabs.find(t => t.value === currentTab)
+    const adminStoreKey = JSON.stringify({ type: "GET_ONE", resource, payload: { id: data.id } })
+    return activeTab?.render({ data, adminKey: adminStoreKey, match, recordID: match.params.id })
+  }
+  return (
+    <Container maxWidth={false}>
+      {renderHeader({ data })}
+      <Spacer mt={2} />
+      <Tabs
+        indicatorColor={"primary"}
+        onChange={(event, value) => {
+          history.push(value)
+        }}
+        scrollButtons="auto"
+        value={currentTab}
+        variant="scrollable"
+      >
+        {tabs.map(tab => (
+          <Tab key={tab.value} label={tab.label} value={tab.value} className={classes.tab} />
+        ))}
+      </Tabs>
+      <Divider className={classes.divider} />
+      <Box mt={2}>{renderCurrentTab()}</Box>
+      <Snackbar state={snackbar} toggleSnackbar={toggleSnackbar} />
+    </Container>
+  )
+}
--- src/views/Members/MemberView.tsx
+++ src/views/Members/MemberView.tsx
@@ -1,95 +1,59 @@
-import { ComponentError, Spacer } from "components"
 import React from "react"
-import { Loading, useQueryWithStore } from "@seasons/react-admin"
-import { Redirect } from "react-router-dom"
-
-import { Box, colors, Container, Divider, Tab, Tabs, Theme } from "@material-ui/core"
-import { makeStyles } from "@material-ui/styles"
 
 import { AccountView } from "./Account"
-import { Header } from "./Header"
 import { HistoryView } from "./History"
 import { MemberViewProps } from "./interfaces"
 import { PersonalView } from "./Personal"
 import { PushNotificationsView } from "./Notifications"
-
-const useStyles = makeStyles<Theme>(theme => ({
-  tabs: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(5),
-  },
-  divider: {
-    backgroundColor: colors.grey[300],
-  },
-  content: {
-    marginTop: theme.spacing(2),
-  },
-}))
+import { DetailView } from "components/DetailView"
+import { Header } from "./Header"
 
 export const MemberView: React.FunctionComponent<MemberViewProps> = ({ match, history, props }) => {
-  const classes = useStyles()
-  const { tab: currentTab, id: memberID } = match.params
-  const tabs = [
-    { value: "account", label: "Account" },
-    { value: "personal", label: "Personal" },
-    { value: "history", label: "History" },
-    { value: "notifs", label: "Notifications" },
-  ]
-
-  const { data, loading, error } = useQueryWithStore({
-    type: "getOne",
-    resource: "Customer",
-    payload: { id: memberID },
-  })
-
-  if (loading) return <Loading />
-
-  if (error || !data) return <ComponentError />
-
-  const handleTabsChange = (event, value) => {
-    history.push(value)
-  }
-
-  if (!currentTab) {
-    return <Redirect to={`/members/${memberID}/account`} />
-  }
-
-  const adminStoreKey = JSON.stringify({ type: "GET_ONE", resource: "Customer", payload: { id: data.id } })
-
   return (
-    <Container maxWidth={false}>
-      <Header history={history} member={data} />
-      <Spacer mt={2} />
-      <Tabs
-        indicatorColor={"primary"}
-        onChange={handleTabsChange}
-        scrollButtons="auto"
-        value={currentTab}
-        variant="scrollable"
-      >
-        {tabs.map(tab => (
-          <Tab key={tab.value} label={tab.label} value={tab.value} className={classes.tab} />
-        ))}
-      </Tabs>
-      <Divider className={classes.divider} />
-      <Box mt={2}>
-        {currentTab === "account" && (
-          <AccountView
-            {...props}
-            match={match}
-            basePath={`/members/${memberID}/account`}
-            member={data}
-            adminKey={adminStoreKey}
-          />
-        )}
-        {currentTab === "personal" && (
-          <PersonalView {...props} basePath={`/members/${memberID}/personal`} member={data} adminKey={adminStoreKey} />
-        )}
-        {currentTab === "history" && <HistoryView {...props} basePath={`/members/${memberID}/history`} member={data} />}
-        {currentTab === "notifs" && (
-          <PushNotificationsView {...props} basePath={`/members/${memberID}/notifs`} member={data} />
-        )}
-      </Box>
-    </Container>
+    <DetailView
+      match={match}
+      history={history}
+      resource={"Customer"}
+      renderHeader={({ data: member }) => {
+        return <Header history={history} member={member} />
+      }}
+      tabs={[
+        {
+          value: "account",
+          label: "Account",
+          render: ({ data, adminKey, match, recordID }) => (
+            <AccountView
+              {...props}
+              member={data}
+              adminKey={adminKey}
+              basePath={`/members/${recordID}/account`}
+              match={match}
+            />
+          ),
+        },
+        {
+          value: "personal",
+          label: "Personal",
+          render: ({ data, adminKey, recordID }) => (
+            <PersonalView {...props} basePath={`/members/${recordID}/personal`} member={data} adminKey={adminKey} />
+          ),
+        },
+        {
+          value: "history",
+          label: "History",
+          render: ({ data, recordID }) => (
+            <HistoryView {...props} basePath={`/members/${recordID}/history`} member={data} />
+          ),
+        },
+        {
+          value: "notifs",
+          label: "Notifications",
+          render: ({ data, recordID }) => (
+            <PushNotificationsView {...props} basePath={`/members/${recordID}/notifs`} member={data} />
+          ),
+        },
+      ]}
+      {...props}
+    />
   )
 }
