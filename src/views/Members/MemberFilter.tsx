a245dfb8af5677bea166b37fbdf63c598170fe2f
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-10-29 18:43:21 -0400

    followup

--- src/views/Members/MemberFilter.tsx
+++ src/views/Members/MemberFilter.tsx
@@ -24,7 +24,15 @@ export const MemberFilter = props => (
       <SelectInput
         label="Authorizations"
         source="admissions.authorizationsCount"
-        choices={[{ id: 0, name: "0" }]}
+        choices={[
+          { id: 0, name: "0" },
+          { id: 1, name: "1" },
+          { id: 2, name: "2" },
+          { id: 3, name: "3" },
+          { id: 4, name: "4" },
+          { id: 5, name: "5" },
+          { id: 6, name: "6" },
+        ]}
         alwaysOn
       />
       <SelectInput
