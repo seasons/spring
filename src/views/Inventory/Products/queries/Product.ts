fe4d88e2d6e093892509f4071840949ef20e9ac3
Kieran Gillen <kgillen@gmail.com>
2021-05-17 13:15:44 -0700

    Updates where

--- src/views/Inventory/Products/queries/Product.ts
+++ src/views/Inventory/Products/queries/Product.ts
@@ -194,7 +194,7 @@ export const PHYSICAL_PRODUCT_STATUSES_QUERY = gql`
 `
 export const PRODUCT_EDIT_QUERY = gql`
   query ProductEditQuery($input: ProductWhereUniqueInput!) {
-    categories(childLeavesOnly: true, orderBy: name_ASC) {
+    categories(where: { children_every: { id: null } }, orderBy: name_ASC) {
       id
       name
     }
@@ -209,7 +209,7 @@ export const PRODUCT_EDIT_QUERY = gql`
 
 export const PRODUCT_UPSERT_QUERY = gql`
   query ProductUpsertQuery {
-    categories(childLeavesOnly: true, orderBy: name_ASC) {
+    categories(where: { children_every: { id: null } }, orderBy: name_ASC) {
       id
       name
     }
