7de432dc8c833a41b87af5b495bece039ef65869
Kieran Gillen <kgillen@gmail.com>
2021-08-10 13:51:51 -0400

    Updates deleteBagItem with type

--- src/views/Members/Bag/BagItemCard.tsx
+++ src/views/Members/Bag/BagItemCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { useRefresh } from "@seasons/react-admin"
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, styled } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardHeader from "@material-ui/core/CardHeader"
 import CardMedia from "@material-ui/core/CardMedia"
@@ -8,8 +8,10 @@ import IconButton from "@material-ui/core/IconButton"
 import { ConfirmationDialog } from "components/ConfirmationDialog"
 import { red } from "@material-ui/core/colors"
 import DeleteIcon from "@material-ui/icons/Delete"
+import ArchiveIcon from "@material-ui/icons/Archive"
 import { useMutation } from "react-apollo"
 import { gql } from "apollo-boost"
+import { Box, Typography } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -36,24 +38,25 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export const REMOVE_FROM_BAG = gql`
-  mutation DeleteBagItem($itemID: ID!) {
-    deleteBagItem(itemID: $itemID)
+  mutation DeleteBagItem($itemID: ID!, $type: DeleteBagItemType) {
+    deleteBagItem(itemID: $itemID, type: $type)
   }
 `
 
 export const BagItemCard = props => {
   const classes = useStyles()
   const refresh = useRefresh()
-  const [isConfirmationDialogOpen, setIsConfirmationDialogOpen] = useState(false)
+  const [isDeleteConfirmationDialogOpen, setIsDeleteConfirmationDialogOpen] = useState(false)
+  const [isReturnConfirmationDialogOpen, setIsReturnConfirmationDialogOpen] = useState(false)
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [deleteBagItem] = useMutation(REMOVE_FROM_BAG, {
     onCompleted: () => {
       refresh()
-      setIsConfirmationDialogOpen(false)
+      setIsDeleteConfirmationDialogOpen(false)
     },
     onError: () => {
       refresh()
-      setIsConfirmationDialogOpen(false)
+      setIsDeleteConfirmationDialogOpen(false)
     },
   })
 
@@ -62,7 +65,7 @@ export const BagItemCard = props => {
   const { name, brand } = product
   const image = product.images?.[0]
 
-  const onCloseConfirmationDialog = async (agreed: boolean) => {
+  const onCloseConfirmationDialog = async (agreed: boolean, type: "Return" | "Delete") => {
     // Make sure user has confirmed submission
     if (!agreed || isSubmitting) {
       return
@@ -72,6 +75,7 @@ export const BagItemCard = props => {
     await deleteBagItem({
       variables: {
         itemID: bagItem.id,
+        type,
       },
     })
     setIsSubmitting(false)
@@ -85,25 +89,53 @@ export const BagItemCard = props => {
           subheader={brand.name}
           action={
             <IconButton
-              aria-label="remove"
+              aria-label="return"
               onClick={() => {
-                setIsConfirmationDialogOpen(true)
+                setIsReturnConfirmationDialogOpen(true)
               }}
             >
-              <DeleteIcon />
+              <ArchiveIcon />
             </IconButton>
           }
         />
         <CardMedia className={classes.media} image={image.url} />
+        <Box padding="0px 16px">
+          <FlexBox>
+            <Typography>{bagItem?.status}</Typography>
+            <IconButton
+              aria-label="delete"
+              onClick={() => {
+                setIsDeleteConfirmationDialogOpen(true)
+              }}
+            >
+              <DeleteIcon fontSize="small" />
+            </IconButton>
+          </FlexBox>
+        </Box>
       </Card>
 
       <ConfirmationDialog
-        title="Remove Item"
-        body="Are you sure you want to remove this bag item?"
-        open={isConfirmationDialogOpen}
-        setOpen={setIsConfirmationDialogOpen}
-        onClose={onCloseConfirmationDialog}
+        title="Return Item"
+        body="Are you sure you want to mark this item as returned?"
+        open={isReturnConfirmationDialogOpen}
+        setOpen={setIsReturnConfirmationDialogOpen}
+        onClose={agreed => onCloseConfirmationDialog(agreed, "Return")}
+      />
+
+      <ConfirmationDialog
+        title="Delete Item"
+        body="Warning: Are you sure you want to delete this bag item? This will also remove the item from it's reservation. You should return the item instead of deleting if this item was fully rented by a customer."
+        open={isDeleteConfirmationDialogOpen}
+        setOpen={setIsDeleteConfirmationDialogOpen}
+        onClose={agreed => onCloseConfirmationDialog(agreed, "Delete")}
       />
     </>
   )
 }
+
+const FlexBox = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  width: "100%",
+  alignItems: "center",
+})
