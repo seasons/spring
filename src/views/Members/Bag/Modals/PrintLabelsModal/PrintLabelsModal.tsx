76d8cbad19b3c6698edc44f855969747748f9e2c
Luc Succes <l2succes@gmail.com>
2021-11-24 15:47:16 -0500

    Move PrintLabelCard to separate module

--- src/views/Members/Bag/Modals/PrintLabelsModal/PrintLabelsModal.tsx
+++ src/views/Members/Bag/Modals/PrintLabelsModal/PrintLabelsModal.tsx
@@ -1,47 +1,13 @@
 import { DialogTitle } from "components"
 
-import { Box, Button, Dialog, DialogContent, DialogActions, Link, Typography, Card, styled } from "@material-ui/core"
-import { Link as RouterLink } from "react-router-dom"
+import { Box, Button, Dialog, DialogContent, DialogActions } from "@material-ui/core"
 import React from "react"
+import { PrintLabelCard } from "./PrintLabelCard"
 
 interface PrintLabelsModalProps {
   open: boolean
   onClose?: () => void
   data: any
-}
-
-const PrintLabelCard = ({ data }) => {
-  const { shippingLabel, direction } = data
-  const { trackingNumber, image, trackingURL } = shippingLabel
-  return (
-    <Card>
-      <FlexBox p={2} display="flex" justifyContent="space-between" alignItems="center">
-        <Box>
-          <Typography variant="h6" style={{ fontWeight: "bold" }}>
-            {direction}
-          </Typography>
-          <Link
-            variant="body1"
-            onClick={e => {
-              window.open(trackingURL, "_blank")
-            }}
-          >
-            <Typography color="secondary">{trackingNumber}</Typography>
-          </Link>
-        </Box>
-        <Box ml={4} my={2}>
-          <Button
-            variant="outlined"
-            onClick={() => {
-              window.open(image, "_blank")
-            }}
-          >
-            Print
-          </Button>
-        </Box>
-      </FlexBox>
-    </Card>
-  )
 }
 
 export const PrintLabelsModal: React.FC<PrintLabelsModalProps> = ({ data, open, onClose }) => {
@@ -71,8 +37,3 @@ export const PrintLabelsModal: React.FC<PrintLabelsModalProps> = ({ data, open, 
     </>
   )
 }
-
-const FlexBox = styled(Box)({
-  height: "100%",
-  width: "auto",
-})
