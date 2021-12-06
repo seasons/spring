595255ecea9476388bc6cb7436af12c2a238fd17
Kieran Gillen <kgillen@gmail.com>
2021-12-06 15:46:57 +0100

    removes log

--- src/views/Members/Bag/BagColumn.tsx
+++ src/views/Members/Bag/BagColumn.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from "@apollo/react-hooks"
 import { Box, Typography, styled, Button } from "@material-ui/core"
 import { Separator, Spacer } from "components"
-import { head, truncate } from "lodash"
+import { truncate } from "lodash"
 import React from "react"
 import { BagItemCard } from "./BagItemCard"
 import { GENERATE_LABELS } from "./mutations"
@@ -15,7 +15,6 @@ export const BagColumn = ({ customer, bagSection, index, setShowModal, setData }
   const [generateLabels] = useMutation(GENERATE_LABELS)
 
   const isForPickup = bagItems.some(item => item?.reservationPhysicalProduct?.shippingMethod?.code === "Pickup")
-  console.log("bagItems", bagItems)
 
   let buttons
   switch (bagSection.id) {
